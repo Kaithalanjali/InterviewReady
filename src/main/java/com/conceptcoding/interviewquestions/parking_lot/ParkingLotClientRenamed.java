b07e870ce7fa429391606db66d99fe085e4f21d4
Anjali kaithal <anjali.kaithal.met19@itbhu.ac.in>
2026-07-26 19:29:20 +0530

    Refactoring

--- src/main/java/com/conceptcoding/interviewquestions/parking_lot/ParkingLotClientRenamed.java
+++ src/main/java/com/conceptcoding/interviewquestions/parking_lot/ParkingLotClientRenamed.java
@@ -18,7 +18,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-public class ParkingLotClient {
+public class ParkingLotClientRenamed {
 
     public static void main(String[] args) {
 
